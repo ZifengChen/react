5ce48764b73e3d271bede85b5b78c677250721f8
ken36chan@gmail.com <chen1677@purdue.edu>
2018-09-09 21:33:51 -0400

    php test

---
+++ h1.php
@@ -0,0 +1,12 @@
+<?php
+$array = [1,2,3,4,5,6];
+$result = 0;
+foreach (array_reverse)($array) as $key => $item; $key++){
+	$result .= $array[$key];
+	$keyIteam = $key + $item;
+}
+
+echo $keyItem;
+?>
+
+
