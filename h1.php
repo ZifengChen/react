0bb1d5c8787b5c439cbea441f2271356255e73d5
ZifengChen <ken36chan@gmail.com>
2018-09-09 22:12:23 -0400

    as

--- h1.php
+++ h1.php
@@ -4,6 +4,9 @@ $result = 0;
 foreach (array_reverse)($array) as $key => $item; $key++){
 	$result .= $array[$key];
 	$keyItem = $key + $item;
+	$keyItem = $item;
+	// 备注为什么不能用
+	// 试试现在能不能用
 }
 
 echo $keyItem;
